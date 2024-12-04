eb78b07ca47fed72771b467807f4e7863180c4dd
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2024-12-04 11:30:40 -0300

    fix: fixed problems with hardFaults occuring

--- Laboratorio_2/lab02_project/main.s
+++ Laboratorio_2/lab02_project/main.s
@@ -1,46 +1,54 @@
-; Exemplo.s
-; Desenvolvido para a placa EK-TM4C1294XL
-; Prof. Guilherme Peron
-; 12/03/2018
-
 ; -------------------------------------------------------------------------------
-        THUMB                        ; Instru��es do tipo Thumb-2
+        THUMB                        ; Instruções do tipo Thumb-2
 ; -------------------------------------------------------------------------------
-; Declara��es EQU - Defines
+; Declarações EQU - Defines
 ;<NOME>         EQU <VALOR>
-TIMER_TIMER0_GPTMICR_R		EQU	   0x40030024
+LABEL_NUM 		EQU 0x0B
+MUL_NUM1		EQU 0x00
+MUL_NUM2		EQU 0x05
+MUL_RES			EQU	0x09
 ; -------------------------------------------------------------------------------
-; �rea de Dados - Declara��es de vari�veis
+; Área de Dados - Declarações de variáveis
 		AREA  DATA, ALIGN=2
-		; Se alguma vari�vel for chamada em outro arquivo
-		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a vari�vel <var> a 
+		; Se alguma variável for chamada em outro arquivo
+		;EXPORT  <var> [DATA,SIZE=<tam>]   ; Permite chamar a variável <var> a 
 		                                   ; partir de outro arquivo
-;<var>	SPACE <tam>                        ; Declara uma vari�vel de nome <var>
+;<var>	SPACE <tam>                        ; Declara uma variável de nome <var>
                                            ; de <tam> bytes a partir da primeira 
-                                           ; posi��o da RAM		
+                                           ; posição da RAM	
+string_label SPACE 16
+string_mul SPACE 12
+
+;arrays
+multipliers SPACE 12
+
+		EXPORT string_label [DATA, SIZE=16]
+		EXPORT string_mul   [DATA, SIZE=12]
 
 ; -------------------------------------------------------------------------------
-; �rea de C�digo - Tudo abaixo da diretiva a seguir ser� armazenado na mem�ria de 
-;                  c�digo
+; Área de Código - Tudo abaixo da diretiva a seguir será armazenado na memória de 
+;                  código
         AREA    |.text|, CODE, READONLY, ALIGN=2
 
-		; Se alguma fun��o do arquivo for chamada em outro arquivo	
-        EXPORT Start                ; Permite chamar a fun��o Start a partir de 
+		; Se alguma função do arquivo for chamada em outro arquivo	
+        EXPORT Start                ; Permite chamar a função Start a partir de 
 			                        ; outro arquivo. No caso startup.s
-		EXPORT 	Timer0A_Handler
 									
-		; Se chamar alguma fun��o externa	
+									
+		; Se chamar alguma função externa	
         ;IMPORT <func>              ; Permite chamar dentro deste arquivo uma 
-									; fun��o <func>
+									; função <func>
 		IMPORT GPIO_Init
 		IMPORT Display_Init
 		IMPORT Issue_data
 		IMPORT Write_to_display
 		IMPORT SysTick_Init
 		IMPORT PLL_Init
-		IMPORT Mult_Table_Init
-		IMPORT 	Timer_Init
-		IMPORT 	Timer_set_count
+		IMPORT Read_keyboard
+		IMPORT Pos_to_char
+		IMPORT SysTick_Wait1us
+		IMPORT Issue_cmd
+		IMPORT Format_strings
 
 ; -------------------------------------------------------------------------------
 ; Função main()
@@ -49,9 +57,10 @@ Start
 	BL SysTick_Init
 	BL GPIO_Init
 	BL Display_Init
+	BL Variables_Init
 	
 	LDR R0, =string_test
-	MOV R1, #13
+	MOV R1, #12
 	BL Write_to_display
 loop
 	BL Read_keyboard
@@ -81,8 +90,9 @@ loop
 	; char -> uint
 	SUB R0, #0x30
 	
+	;multiplier = multipliers[num]
 	LDR R3, =multipliers
-	LDRB R1, [R3]
+	LDRB R1, [R3, R0]
 	
 	MUL R2, R0, R1
 	
@@ -93,112 +103,156 @@ loop
 	;Insere valores de R0, R1, e R2 nas strings
 	BL Format_strings
 	
+	;atualiza valor do multiplicador
+	;se atingir 10, o valor retorna a 1
+	ADD R1, #0x01
+	CMP R1, #0x0A
+	MOVGE R1, 0x00
+	
+	STRB R1, [R3, R0]
+	
+	;Limpa display
+	MOV R0, #0x01
+	BL Issue_cmd
+	
+	MOV R0, #1640
+	BL SysTick_Wait1us
+	
+	
+	;Retorna cursor para primera linha
+	MOV R0, #0x02
+	BL Issue_cmd
+	
+	MOV R0, #1640
+	BL SysTick_Wait1us
+	
 	LDR R0, =string_label
-	MOV R1, #0xFFFF
+	MOV R1, #16
 	BL Write_to_display
 	
+	;move cursor para segunda linha
+	MOV R0, #0xC0
+	BL Issue_cmd
+	
+	MOV R0, #1640
+	BL SysTick_Wait1us
+	
+	
 	LDR R0, =string_mul
+	MOV R1, #12
 	BL Write_to_display
 	
 	
 	B loop
 
-; -------------------------------------------------------------------------------
-;Format_strings()
-;
-; Entrada:
-;	R0 -> numero pressinado (int)
-;	R1 -> multiplicador (int)
-;	R2 -> resultado da multiplicacao (int)
-;
-; Saida:
-;	Nao ha
-Format_strings
-	PUSH{R3-R6}
-	
-	;Insere R0 na label
-	LDR R3, =string_label
-	ADD R4, R0, #0x30
-	STRB R4, [R3, #LABEL_NUM]
-	
-	;Insere R0, R1 e R2 em string_mul
-	LDR R3, =string_mul
-	BL int_to_string
-	
-	STRB R4, [R3, #MUL_NUM1]
-	ADD R4, R1, #0x30
-	STRB R4, [R3, #MUL_NUM2]
-	
-	LSR R4, R5, #0x08
-	
-	CMP R5, #'0'
-	BEQ load_lsd
-	
-	ADD R6, R3, #MUL_RES
-	STRB R5, [R6], #1
-load_lsd
-	
-	;filtra lsd char
-	AND R4, R5, #0x0F
-	STRB R4, [R6], #1
-	
-	;insere \0 char
-	MOV R4, #'\0'
-	STRB R4, [R6]
-	
-	POP{R3-R6}
-	BX LR
 
 ; -------------------------------------------------------------------------------
-;int_to_string()
-; 
-; Descricao:
-;	Converte o numero decimal de ate dois digitos em R2
-;	Para uma string, guardada em R6.
-;
-; Entrada:
-;	R2 -> numero
-;
-; Saida:
-;	R5 -> string correspondente
-int_to_string
-	PUSH{R0, R1, R3}
-	MOV R5, #0x0
+; Init_variables()
+Variables_Init
+	PUSH{R0, R1}
+
+	;Init string_mul
+	;string_mul <- "* x * = *\0\0\0"
+	LDR R0, =string_mul
 	
-	;Obtem MSD
-	MOV R3, #0x0A
-	UDIV R0, R2, R3
-	ADD R5, R0, #0x30
-	LSL R5, #0x08
+	MOV R1, #'*'
+	STRB R1, [R0], #0x01
 	
-	;Obtem LSD
-	SUB R1, R2, R0
-	ADD R1, #0x030
-	ORR R5, R1
+	MOV R1, #' '
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'x'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #' '
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'*'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #' '
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'='
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #' '
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'*'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'\0'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'\0'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'\0'
+	STRB R1, [R0], #0x01
+	
+	;Init string_label
+	;string_label <- "Tabuada do *\0"
+	LDR R0, =string_label
+	
+	MOV R1, #'T'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'a'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'b'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'u'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'a'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'d'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'a'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #' '
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'d'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'o'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #' '
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'*'
+	STRB R1, [R0], #0x01
+	
+	MOV R1, #'\0'
+	STRB R1, [R0], #0x01
+	
+	;Init multipliers
+	LDR R0, =multipliers
+	ADD R1, R0, #0x0A
+	MOV R3, #0x00
+load_zero
+	STRB R3, [R0], #0x01
+	
+	CMP R0, R1
+	BLO load_zero
 	
 	POP{R0, R1}
 	BX LR
 
 ; -------------------------------------------------------------------------------
-;Timer0A_Handler
-Timer0A_Handler
-	LDR R1, =TIMER_TIMER0_GPTMICR_R
-	MOV R0, #1
-	STR R0, [R1]
-	BX LR
+;Constantes
+
+string_test DCB "Hello World!\0"
 
 ; -------------------------------------------------------------------------------
-;Variaveis
-
-;strings
-string_test DCB "Hello World!/0"
-string_label DCB "Tabuado do */0"
-string_mul DCB "* x * = *\0\0\0"
-
-;arrays
-multipliers DCB 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0
-
-; -------------------------------------------------------------------------------
-;END
-    ALIGN                           ; garante que o fim da se��o est� alinhada 
+;FIM
+    ALIGN                           ; garante que o fim da seção está alinhada 
     END                             ; fim do arquivo
