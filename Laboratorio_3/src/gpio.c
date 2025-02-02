cf887c887c08cfa0c68a05837c749c52310f5d18
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-02 20:41:18 -0300

    feat: Implemented PORTInitGeneric in C

--- Laboratorio_3/src/gpio.c
+++ Laboratorio_3/src/gpio.c
@@ -5,12 +5,25 @@
 
 
 #include <stdint.h>
-
 #include "../lib/tm4c1294ncpdt.h"
 
   
 #define GPIO_PORTJ  (0x0100) //bit 8
 #define GPIO_PORTN  (0x1000) //bit 12
+
+//OFFSETS
+#define GPIO_DATA_OFF					   0x3FC 
+#define GPIO_AMSEL_OFF					 0x528
+#define GPIO_PCTL_OFF					   0x52C
+#define GPIO_DIR_OFF					   0x400
+#define GPIO_AFSEL_OFF					 0x420
+#define GPIO_DEN_OFF					   0x51C
+#define GPIO_PUR_OFF					   0x510
+#define GPIO_IS_OFF						   0x404
+#define GPIO_IBE_OFF					   0x408
+#define GPIO_IEV_OFF					   0x40C
+#define GPIO_IM_OFF						   0x410
+#define GPIO_ICR_OFF					   0x41C
 
 // -------------------------------------------------------------------------------
 // Função GPIO_Init
@@ -73,6 +86,35 @@ void PortN_Output(uint32_t valor)
     //agora vamos fazer o OR com o valor recebido na função
     temp = temp | valor;
     GPIO_PORTN_DATA_R = temp; 
+}
+
+void PORTInitGeneric(
+	volatile uint32_t *base_address,
+	uint8_t sysctl_port_bit,
+	uint32_t io_map,
+	uint32_t pin_map
+){
+	//Habilita Clock na placa
+	SYSCTL_RCGCGPIO_R |= 1 << sysctl_port_bit;
+	while((SYSCTL_PRGPIO_R & (1 << sysctl_port_bit)) != (1 << sysctl_port_bit));
+	
+	//Desabilta funcoes analogicas na porta
+	volatile uint32_t* port_amsel_r = (base_address + GPIO_AMSEL_OFF);
+	*port_amsel_r = 0x0;
+	
+	//Desabilita Funcoes alternativas na porta
+	volatile uint32_t* port_afsel_r = (base_address + GPIO_AFSEL_OFF);
+	volatile uint32_t* port_pctl_r = (base_address + GPIO_PCTL_OFF);
+	*port_pctl_r = 0x0;
+	*port_afsel_r = 0x0;
+	
+	//Define pinos de entrada e saida
+	volatile uint32_t* port_dir_r = (base_address + GPIO_DIR_OFF);
+	*port_dir_r = io_map;
+	
+	//Habilita funcoes digitais nos pinos
+	volatile uint32_t* port_den_r = (base_address + GPIO_DEN_OFF);
+	*port_den_r = pin_map;
 }
 
 
