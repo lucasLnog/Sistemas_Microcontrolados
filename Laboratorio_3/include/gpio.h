4bbfe85ca79fd540c79a8e9a3b81c2169f25989e
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-04 00:35:24 -0300

    add: Added more defines on gpio.h

--- Laboratorio_3/include/gpio.h
+++ Laboratorio_3/include/gpio.h
@@ -1,8 +1,30 @@
 #include <stdint.h>
 
-#define GPIO_PORTJ  (0x0100) //bit 8
-#define GPIO_PORTN  (0x1000) //bit 12
-#define GPIO_PORTP	(0xD)
+
+//GPIO SYSCTL MAPING
+#define GPIO_PORTA		0x0
+#define GPIO_PORTB		0x1
+#define GPIO_PORTJ	  0x8
+#define GPIO_PORTN	  0xC
+#define GPIO_PORTQ		0xE
+#define GPIO_PORTK    0x9
+#define GPIO_PORTM		0xB
+#define GPIO_PORTL		0xA
+#define GPIO_PORTP	 (0xD)
+#define GPIO_PORTH	 (0x7)
+
+//PORT BASES
+#define GPIO_PORTA_AHB				     0x40058000
+#define GPIO_PORTB_AHB					   0x40059000
+#define GPIO_PORTJ_AHB					   0x40060000
+#define GPIO_PORTN_AHB					   0x40064000
+#define GPIO_PORTP_AHB					   0x40065000
+#define GPIO_PORTQ_AHB					   0x40066000
+#define GPIO_PORTK_AHB					   0x40061000
+#define GPIO_PORTM_AHB					   0x40063000
+#define GPIO_PORTL_AHB					   0x40062000
+#define GPIO_PORTH_AHB					   0x4005F000
+
 
 //OFFSETS
 #define GPIO_DATA_OFF					   0x3FC 
@@ -21,7 +43,7 @@
 void GPIO_Init(void);
 
 void PortInitGeneric(
-	volatile uint32_t *base_address,
+	uint32_t base_address,
 	uint8_t sysctl_port_bit,
 	uint32_t io_map,
 	uint32_t pin_map
