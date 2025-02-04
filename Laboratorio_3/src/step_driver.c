9575d24d7d97040f186bd2068c0c7c7482464f2f
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-04 07:46:17 -0300

    feat: Added half-step functionallity

--- Laboratorio_3/src/step_driver.c
+++ Laboratorio_3/src/step_driver.c
@@ -6,46 +6,75 @@
 
 #define ABS(x) (x > 0? x : -x)
 
+uint8_t half_step_seq [8]= {
+	0b0001,
+	0b0011,
+	0b0010,
+	0b0110,
+	0b0100,
+	0b1100,
+	0b1000,
+	0b1001
+};
+
+uint8_t full_step_seq [4]= {
+	0x1,
+	0x2,
+	0x4,
+	0x8,
+};
+
 
 //motor de passo utilizado: 28BYJ-48
 //2048 steps/rev
 //O driver do motor esta conectado nos pinos PH0-3
-void pb_step(int16_t steps){
+void pb_step(int16_t steps, uint8_t half_step){
 	//sentido horario
 	if(steps > 0){
-		stepClockWise(steps);
+		stepClockWise(steps, half_step);
 	}
 	//executa passos no sentido anti-horario
 	else{
-		stepAntiClockWise(steps);
+		stepAntiClockWise(steps, half_step);
 	}
 }
 
 
-void pb_stepDegrees(int16_t degrees){
+void pb_stepDegrees(int16_t degrees, uint8_t half_step){
 	//Convert degrees to steps
 	int16_t steps = degrees * 10;
-	pb_step(steps);
+	pb_step(steps, half_step);
 }
 
-void stepClockWise(int16_t steps){
+void stepClockWise(int16_t steps, uint8_t half_step){
+	uint8_t *data = full_step_seq;
+	uint8_t seq_len = 4;
+	
+	if(half_step){
+		data = half_step_seq;
+		seq_len = 8;
+	}
 	for(uint16_t i = 0; i < steps; i++){
-		uint32_t data = 0x1;
-		while(data != 0x10){
-			GPIO_PORTH_AHB_DATA_R = data;
-			data = data << 1;
+		for(uint8_t j = 0; j < seq_len; j++){
+			GPIO_PORTH_AHB_DATA_R = data[j];
 			//espera...
 			SysTick_Wait1ms(10);
 		}
 	}
 }
 
-void stepAntiClockWise(int16_t steps){
+void stepAntiClockWise(int16_t steps, uint8_t half_step){
+	uint8_t *data = full_step_seq;
+	uint8_t seq_len = 4;
+	
+	if(half_step){
+		data = half_step_seq;
+		seq_len = 8;
+	}
+	
 	for(int16_t i = 0; i > -steps; i++){
-		uint32_t data = 0x8;
-		while(data != 0x0){
-			GPIO_PORTH_AHB_DATA_R = data;
-			data = data >> 1;
+		for(uint8_t j = seq_len - 1; j >= 0; j--){
+			GPIO_PORTH_AHB_DATA_R = data[j];
 			//espera...
 		}
 	}
