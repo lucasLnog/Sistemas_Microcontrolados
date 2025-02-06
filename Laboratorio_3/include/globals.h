19d29c28b5f731061e37fb065bf6933e2248c919
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-05 23:36:25 -0300

    feat: created step_motor_pos and turn_count gloabal variables

--- Laboratorio_3/include/globals.h
+++ Laboratorio_3/include/globals.h
@@ -1,3 +1,7 @@
 #include<stdint.h>
 
 extern uint8_t step_mode;
+
+extern uint8_t step_motor_pos;
+
+extern uint8_t turn_count;
--- Laboratorio_3/src/globals.c
+++ Laboratorio_3/src/globals.c
@@ -1,3 +1,7 @@
 #include "../include/globals.h"
 
 uint8_t step_mode = 0;
+
+uint8_t step_motor_pos;
+
+uint8_t turn_count;
