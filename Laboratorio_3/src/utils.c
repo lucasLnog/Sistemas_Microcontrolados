33a111a2206b458a8caab5d3fa562289022a53f0
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-06 11:16:49 -0300

    feat: Implemented lg2 function and more

--- Laboratorio_3/src/utils.c
+++ Laboratorio_3/src/utils.c
@@ -3,11 +3,20 @@
 #include "../include/globals.h"
 #include "../include/timer.h"
 
+uint32_t lg2(uint32_t x) {
+	uint32_t ans = 0;
+	while (x > 1) {
+		ans++;
+		x /= 2;
+	}
+	return ans;
+}
+
 int32_t readDegsFromKB() {
     uint32_t stable_reads = 0;
 
     uint32_t last_read = Read_keyboard(); 
-    for (int i = 0; i < 5; i++) {
+    for (uint8_t i = 0; i < 5; i++) {
         waitMs(10);
         uint32_t val = Read_keyboard(); 
         if (val != last_read) {
@@ -15,8 +24,8 @@ int32_t readDegsFromKB() {
         }
     }
     uint32_t val = last_read; 
-    uint32_t col = val & 0x0F;
-    uint32_t row = val & 0xF0;
+    uint32_t col = lg2((val >> 4) & 0x0F);
+    uint32_t row = lg2(val & 0x0F);
     if (col > 3 || row > 2) {
         return 0;
     }
@@ -31,24 +40,26 @@ void writeRotToDisplay(int rotation_amount) {
     }
     int turns = (rotation_amount/360) % 100;
     int degrees = rotation_amount%360;
-    char turns_string[] = "Voltas:    , Graus:     ";
+    char turns_string[] = "Voltas:    ";
+		char degree_string[] = " Graus:      PC";
     turns_string[10] = turns%10 + '0';
     turns_string[9] = turns/10 + '0';
-    turns_string[23] = degrees%10 + '0';
-    turns_string[22] = (degrees/10)%10 + '0';
-    turns_string[21] = degrees/100 + '0';
+    degree_string[11] = degrees%10 + '0';
+    degree_string[10] = (degrees/10)%10 + '0';
+    degree_string[9] = degrees/100 + '0';
     if (anti_clockwise) {
-        turns_string[8] = '-';
-        turns_string[20] = '-';
+				if (turns != 0)
+					turns_string[8] = '-';
+        if (degrees != 0)
+					degree_string[8] = '-';
     }
-    char speed_string[] = "PC";
     if (step_mode == 1) {
-        speed_string[0] = 'M';
-        speed_string[1] = 'P';
+        degree_string[13] = 'M';
+        degree_string[14] = 'P';
     }
     Clear_display();
-    Write_to_display(turns_string, 24);
+    Write_to_display(turns_string, 11);
     Break_line();
-    Write_to_display(speed_string, 2);
+    Write_to_display(degree_string, 15);
 }
 
