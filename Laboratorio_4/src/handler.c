58e5ebedad7d1e1552083301cf7902cf90f9b75a
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-11 23:21:36 -0300

    rem: Removed deleted global variables

--- Laboratorio_4/src/handler.c
+++ Laboratorio_4/src/handler.c
@@ -8,11 +8,9 @@ void GPIOPortJ_Handler(){
 	//Verifica quais interrupcoes foram acionadas (tratando todas)
 	if(GPIO_PORTJ_AHB_MIS_R & 0x01){
 		//Altera modo de passo
-		step_mode ^= 0x1;
 	}
 	if(GPIO_PORTJ_AHB_MIS_R & 0x02){
 		//Reseta posicao e contagem de voltas
-		step_motor_pos = 0x0;
 		//turn_count = 0x0;
 	}
 	
