84c6e39925e356129b30e103fb60a6ba9f04e07a
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-11 23:06:12 -0300

    doc: Wrote simple comments explaning the workings of uart functions

--- Laboratorio_4/include/uart.h
+++ Laboratorio_4/include/uart.h
@@ -2,6 +2,12 @@
 
 void UART_Init();
 
+//Envia a mensagem contida num buffer. 
+//Se a FIFO de TX estiver cheia, a funcao para.
+//Retorna a quantidade de caracteres enviados
 uint32_t send_message(char* buffer, uint32_t size);
 
+//Le fila de entrada da UART ate que o buffer esteja cheio ou
+//A fila esteja vazia.
+//Retorna a quantidade de caracteres lidos
 uint32_t read_message(char* buffer, uint32_t size);
