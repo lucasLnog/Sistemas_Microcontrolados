2349a0ea11d7d318b9675d88713d2c947d9a5499
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-13 04:00:15 -0300

    feat: Implemented dc_motor_timer_init

--- Laboratorio_4/src/dc_motor.c
+++ Laboratorio_4/src/dc_motor.c
@@ -8,6 +8,8 @@
 
 void motor_pins_init();
 void pwm_init();
+void dc_motor_timer_init();
+void pwm_gen_init();
 
 /* ============================= FUNCTION IMPLEMENTATIONS ============================= */
 
@@ -37,7 +39,42 @@ void motor_pins_init(){
     GPIO_PORTE_AHB_DATA_R &= ~(0x03);
 }
 
-//inicializa Timer 1 para o pwm
+
 void pwm_init(){
+
+}
+
+//inicializa Timer 1A para o pwm
+void dc_motor_timer_init(){
+    //Habilita clock no periferico
+    SYSCTL_RCGCTIMER_R |= SYSCTL_RCGCTIMER_R1;
+    while(!(SYSCTL_PRTIMER_R & SYSCTL_PRTIMER_R1));
+
+    //desabilita timer 1A
+    TIMER1_CTL_R &= ~TIMER_CTL_TAEN;
+
+    // modo de 16 bits
+    TIMER1_CFG_R = 0x04;
+
+    //periodico, pwm e sem captura
+    TIMER1_TAMR_R = TIMER_TAMR_TAMR_PERIOD | TIMER_TAMR_TAAMS;
+
+    //carrega prescaler
+    // T = 1ms & clk = 80MHz => 80_000 ticks
+    //prescaler = 2, ILR = 40_000
+    TIMER1_TAPR_R = 2;
+    TIMER1_TAILR_R = 40000;
+    TIMER1_TAMATCHR_R = 0;
+
+    //Habilita Interrupcoes
+    TIMER1_TAMR_R |= TIMER_TAMR_TAPWMIE;
+    NVIC_EN0_R |= (1 << 21);
+
+    //Habilita timer 1A
+    TIMER1_CTL_R |= TIMER_CTL_TAEN;
+}
+
+// Inicializa o gerador de pwm 0 com f = 1kHz (T = 1ms)
+void pwm_gen_init(){
 
 }
