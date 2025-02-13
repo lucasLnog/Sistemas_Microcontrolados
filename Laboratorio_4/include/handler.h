6eb1780e3d4f2783c79a6d4f8919f99031c22d37
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-13 11:46:53 -0300

    feat: Implemented handler for pwm interruptions

--- Laboratorio_4/include/handler.h
+++ Laboratorio_4/include/handler.h
@@ -1,2 +1,4 @@
 
 void ADC0Seq3_Handler();
+
+void Timer1A_Handler();
--- Laboratorio_4/src/handler.c
+++ Laboratorio_4/src/handler.c
@@ -1,6 +1,7 @@
 #include<stdint.h>
 #include "../include/handler.h"
 #include "../include/globals.h"
+#include "../include/dc_motor.h"
 #include "../lib/tm4c1294ncpdt.h"
 
 
@@ -10,11 +11,16 @@ void ADC0Seq3_Handler(){
 }
 
 void Timer1A_Handler(){
-	if(TIMER1_MIS_R & TIMER_MIS_TAMMIS){
-		GPIO_PORTN_DATA_R = 0x01;
+	if(TIMER1_MIS_R & TIMER_MIS_TATOMIS){
+		GPIO_PORTE_AHB_DATA_R &= ~(0x03);
+		GPIO_PORTN_DATA_R &= ~(0x01);
 	}
-	else if(TIMER1_MIS_R & TIMER_MIS_TATOMIS){
-		GPIO_PORTN_DATA_R = 0x00;
+	else if(TIMER1_MIS_R & TIMER_MIS_TAMMIS){
+		if(get_motor_dir())
+			GPIO_PORTE_AHB_DATA_R |= 0x01;
+		else
+			GPIO_PORTE_AHB_DATA_R |= 0x02;
+		GPIO_PORTN_DATA_R |= 0x01;
 	}
 	TIMER1_ICR_R = TIMER_ICR_TATOCINT | TIMER_ICR_TAMCINT;
 }
