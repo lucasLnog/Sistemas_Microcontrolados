e3c3761cc8dad365cfd30e5e2596864e8ac9114f
Henrique Bubniak <hg.bubniak@gmail.com>
2025-02-13 11:50:02 -0300

    feat: updated main function

--- Laboratorio_4/src/main.c
+++ Laboratorio_4/src/main.c
@@ -5,23 +5,40 @@
 #include "../include/timer.h"
 #include "../include/globals.h"
 #include "../include/uart.h"
+#include "../include/state_machine.h"
+#include "../include/adc.h"
+#include "../include/app_utils.h"
 
-char hello [] = "Hello World!\n";
+
 
 int main(void)
 {
 	PLL_Init();
 	timerInit();
 	SysTick_Init();
-	//GPIO_Init();
 	UART_Init();
-	//Display_Init();
+    adc_init();
+    adc_disable_int();
 	while (1){
+        print_state_messsage();
+        int32_t new_speed = -1;
+        int8_t new_direction = -1;
 		uint32_t read_count = 0;
-		while(!read_count ){
-			read_count = read_message(rx_buffer, 50);
-		}
-		send_message(hello, 13);
-		waitMs(500);
+        char message;
+        read_count = read_message(&message, 1);
+        if (read_count == 0) {
+            if (state == Potentiometer) {
+                speed_read_potentiometer(&new_speed, &new_direction);
+            }
+        } else {
+            parse_message(message, &new_speed, &new_direction);
+            if (new_speed == -1) {
+                new_speed = get_motor_speed();
+            }
+            if (new_direction == -1) {
+                new_direction = get_motor_direction();
+            }
+        }
+        set_motor_speed(new_speed, new_direction);
 	}
 }
