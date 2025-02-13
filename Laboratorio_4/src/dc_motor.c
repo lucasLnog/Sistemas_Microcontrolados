783be696c427ad0d45ede28e4c6158197232b66d
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-13 02:56:59 -0300

    feat: Implemented motor_pins_init

--- Laboratorio_4/src/dc_motor.c
+++ Laboratorio_4/src/dc_motor.c
@@ -1,6 +1,7 @@
 #include <stdint.h>
 #include "../lib/tm4c1294ncpdt.h"
 #include "../include/dc_motor.h"
+#include "../include/gpio.h"
 
 /* ============================ AUX FUNCTIONS DECLARATIONS ============================ */
 
@@ -17,7 +18,23 @@ void dc_motor_init(){
 
 //Inicializa os pinos PF2, PE0 e PE1
 void motor_pins_init(){
+    //Inicializa Port F PF2 (output)
+    PortInitGeneric(
+        (uint32_t) GPIO_PORTF_AHB_DATA_BITS_R,
+        GPIO_PORTF,
+        0x04,
+        0x04
+    );
+    GPIO_PORTF_AHB_DATA_R |= 0x04;
 
+    //Inicializa Port E PE0 e PE1 (outputs)
+    PortInitGeneric(
+        (uint32_t) GPIO_PORTE_AHB_DATA_BITS_R,
+        GPIO_PORTE,
+        0x03,
+        0x03
+    );
+    GPIO_PORTE_AHB_DATA_R &= ~(0x03);
 }
 
 //inicializa Timer 1 para o pwm
