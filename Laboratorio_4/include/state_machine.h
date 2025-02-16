3c34d5f3ca7431acd21a4f63df6ce428b2c6b9c8
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-16 18:48:14 -0300

    feat: Created state_machine.h and state_machine.c. implemented exec_startup_state

---
+++ Laboratorio_4/include/state_machine.h
@@ -0,0 +1,9 @@
+
+enum State {
+	STARTUP,
+	CLI_CTL,
+	POT_CTL,
+};
+
+
+enum State switch_state(char input);
---
+++ Laboratorio_4/src/state_machine.c
@@ -0,0 +1,45 @@
+#include <stdint.h>
+#include "../include/state_machine.h"
+#include "../include/dc_motor.h"
+#include "../include/uart.h"
+#include "../include/timer.h"
+#include "../include/globals.h"
+
+/* ========================== GLOBAL VARIABLES DECLARATIONS =========================== */
+
+enum State cur_state = STARTUP;
+
+char startup_str [] = "Motor parado, pressione \'*\' para iniciar.\n\r";
+uint32_t startup_str_len = sizeof(startup_str)/sizeof(char);
+
+/* ============================ AUX FUNCTIONS DECLARATIONS ============================ */
+
+void exec_startup_state();
+void exec_clictl_state();
+void exec_potctl_state();
+
+/* ============================= FUNCTION IMPLEMENTATIONS ============================= */
+
+void exec_state();
+
+/* =========================== AUX FUNCTIONS IMPLEMENTATIONS ========================== */
+
+void exec_startup_state(){
+	set_motor_speed(0, 0);
+	uint16_t proceed = 0;
+	
+	
+	send_message(startup_str, startup_str_len);
+	while(!proceed){
+		uint16_t read_count = 0;
+		read_count = read_message(rx_buffer, 5);
+		if(read_count){
+			proceed = 1;
+		}
+	}
+	switch_state(rx_buffer[0]);	
+}
+
+void exec_clictl_state(){
+	
+}
