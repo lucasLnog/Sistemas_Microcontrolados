d39ed1f75e681b12b3fb4da3b0eade2d44df7a1b
Henrique  Ribas Zambenedetti <hique.rz03@gmail.com>
2025-02-16 22:23:11 -0300

    fix: Removed led blinking in Timer1A_Handler

--- Laboratorio_4/src/handler.c
+++ Laboratorio_4/src/handler.c
@@ -13,14 +13,12 @@ void ADC0Seq3_Handler(){
 void Timer1A_Handler(){
 	if(TIMER1_MIS_R & TIMER_MIS_TATOMIS){
 		GPIO_PORTE_AHB_DATA_R &= ~(0x03);
-		GPIO_PORTN_DATA_R &= ~(0x01);
 	}
 	else if(TIMER1_MIS_R & TIMER_MIS_TAMMIS){
 		if(get_motor_dir())
 			GPIO_PORTE_AHB_DATA_R |= 0x01;
 		else
 			GPIO_PORTE_AHB_DATA_R |= 0x02;
-		GPIO_PORTN_DATA_R |= 0x01;
 	}
 	TIMER1_ICR_R = TIMER_ICR_TATOCINT | TIMER_ICR_TAMCINT;
 }
